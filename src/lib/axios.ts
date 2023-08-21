3600876c3bc9c0aca29f3eff33778e129d211427
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-08-21 15:13:56 -0300

    Detalhe final: Faltou o Secure na URL

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const apiGitHubUser = axios.create({
-  baseURL: 'http://api.github.com'
+  baseURL: 'https://api.github.com'
 });
 
 export const apiGitHubIssueSearch = axios.create({
